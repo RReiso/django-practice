1b52603641b2029f6b0cefe00410f3e019c0ed7a
Ruta Reisoglu <rutareiso@protonmail.com>
2022-09-08 14:31:31 -0400

    Create Student model

--- students/models.py
+++ students/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Student(models.Model):
+    section_choice = (
+        ('A', 'SectionA'),
+        ('B', 'SectionB'),
+    )
+    roll_no = models.IntegerField()
+    photo = models.ImageField(upload_to='photo/%Y/%m/%d/')
+    full_name = models.CharField(max_length=200)
+    course = models.CharField(max_length=100)
+    grade = models.CharField(max_length=100)
+    section = models.CharField(choices=section_choice, max_length=1)
